f9a082b42b4fcef2e819a191515c2bb756a3ea79
Juan Alvarez <juanandres140299@gmail.com>
2024-03-21 22:45:00 -0400

    add initial theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: { DEFAULT: "var(--primary)" },
+        foreground: { DEFAULT: "var(--foreground)" },
+        background: { DEFAULT: "var(--background)" },
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", "sans"],
       },
     },
   },
+  darkMode: "class",
   plugins: [],
 };
 export default config;
